6b9bbe8c69a347e05e5734d95a1f29b1a3f09e88
Daniel Penedo <pen22217@uvg.edu.gt>
2025-02-11 23:38:21 -0600

    Arreglar comentarios

--- Starters/functions.py
+++ Starters/functions.py
@@ -1,13 +1,13 @@
-# def nombre_cliente(nombre):
-#     return nombre
+def nombre_cliente(nombre):
+    return nombre
     
-# def habitacion(habitaciones):
-#     return habitaciones
+def habitacion(habitaciones):
+    return habitaciones
     
-# def desayuno(desayunos):
-#     return desayunos
+def desayuno(desayunos):
+    return desayunos
     
-# def registro_cliente(nombre, habitaciones, desayunos):
-#     return "Bienvenido, " + nombre_cliente(nombre) + " Ha reservado una " + habitacion(habitaciones) + " con " + desayuno(desayunos)
+def registro_cliente(nombre, habitaciones, desayunos):
+    return "Bienvenido, " + nombre_cliente(nombre) + " Ha reservado una " + habitacion(habitaciones) + " con " + desayuno(desayunos)
 
-# print(registro_cliente(nombre="Daniel", habitaciones="Suite", desayunos="desayuno continental"))
+print(registro_cliente(nombre="Daniel", habitaciones="Suite", desayunos="desayuno continental"))
